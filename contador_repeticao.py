316aee9113a628b6d04beda20a43327a07ef69d9
Lucas Bispo <lucasdefsbispo@gmail.com>
2026-02-05 21:41:07 -0300

    Aula_05

---
+++ contador_repeticao.py
@@ -0,0 +1,33 @@
+'''
+
+O que é uma estrutura de repetição?
+
+-> Serve para executar um bloco de código várias vezes
+-. Enquanto uma condição for verdadeira
+
+
+'''
+contador = 10
+
+
+while contador >=0:
+    print(contador)
+    contador = contador - 1
+
+'''
+Qual a logica de uma taboada? Quero dazer a tabuada do 5 e deve mostrar 
+
+
+
+5 X 1 = 5
+5 X 2 = 10
+
+
+'''
+tabuada = 5 
+multiplicado = 0
+
+while multiplicado <= 10:
+    print(tabuada," X ", multiplicado, "=", multiplicado * tabuada)
+    multiplicado += 1
+ 
